5212819af00818912aabade5086987bab68300bc
hojoon90 <hojoon90@gmail.com>
2024-01-19 23:46:36 +0900

    Initial commit

---
+++ src/test/java/com/bot/telegram/TelegramReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.telegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
