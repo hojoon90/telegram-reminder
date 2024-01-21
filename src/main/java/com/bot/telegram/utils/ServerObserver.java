06ddcbec1aea2251ad4e21a956940acfb1d32762
hojoon90 <hojoon90@gmail.com>
2024-01-21 23:47:53 +0900

    공통화 작업중

--- src/main/java/com/bot/telegram/utils/ServerObserver.java
+++ src/main/java/com/bot/telegram/utils/ServerObserver.java
@@ -4,8 +4,10 @@ import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.nio.charset.Charset;
+import java.util.List;
 
 @Component
 public class ServerObserver {
@@ -20,48 +22,52 @@ public class ServerObserver {
 
     //TODO 공통화 고민
     public String checkServerList(String send_txt) {
-
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
-        HttpEntity entity = new HttpEntity(null, httpHeaders);
-        RestTemplate restTemplate  = new RestTemplate();
-        String sendUrl = "";
 
-        for (String url : checkUrl) {
-            try{
-                sendUrl = url.split(",")[1] ;
-                ResponseEntity<String> responseEntity = restTemplate.exchange(sendUrl, HttpMethod.GET, entity, String.class);
-                if (responseEntity.getStatusCode().value() == 200) {
-                    send_txt += url.split(",")[0] + " : success \n";
-                } else {
-                    send_txt += url.split(",")[0] + " : fail \n";
-                }
-
-            } catch (Exception e) {
-                send_txt += url + " service : fail \n";
-            }
-        }
+        send_txt = makeStatusMessage(send_txt, checkUrl);
         return send_txt;
     }
 
     //TODO 공통화 고민
     public String checkIpList(String send_txt) {
-        String sendIp = "";
+        send_txt = makeStatusMessage(send_txt, checkIp);
+        return send_txt;
+    }
 
-        for (String ip : checkIp) {
+    private boolean checkServerAlive(String sendUrl, HttpEntity entity, RestTemplate restTemplate) throws IOException {
+        ResponseEntity<String> responseEntity = restTemplate.exchange(sendUrl, HttpMethod.GET, entity, String.class);
+        boolean isAlive = responseEntity.getStatusCode().value() == HttpStatus.OK.value();
+
+        return isAlive;
+    }
+
+    private boolean checkIpAlive(String sendIp) throws IOException {
+        InetAddress pingCheck = InetAddress.getByName(sendIp);
+        boolean isAlive = pingCheck.isReachable(1000);
+
+        return isAlive;
+    }
+
+    public String makeStatusMessage(String send_txt, String[] list) {
+        String temp = "";
+
+        for (String source : list) {
             try {
-                sendIp = ip.split(",")[1] ;
-                InetAddress pingCheck = InetAddress.getByName(sendIp);
+                temp = source.split(",")[1] ;
+                InetAddress pingCheck = InetAddress.getByName(temp);
                 boolean isAlive = pingCheck.isReachable(1000);
                 if (isAlive) {
-                    send_txt += ip.split(",")[0] + " : success \n";
+                    send_txt += source.split(",")[0] + " : success \n";
                 } else {
-                    send_txt += ip.split(",")[0] + " : fail \n";
+                    send_txt += source.split(",")[0] + " : fail \n";
                 }
             } catch (Exception e) {
-                send_txt += ip + " service : fail \n";
+                send_txt += source + " service : fail \n";
             }
         }
+
+
 
         return send_txt;
     }
